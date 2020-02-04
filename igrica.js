bdee592f665d2f7da013d7af4bb03a2e48593577
Slaven B <slaven.bunijevac@gmail.com>
2020-02-04 17:44:42 +0100

    unio igricu

---
+++ igrica.js
@@ -0,0 +1,80 @@
+const reset = document.getElementById('reset');
+const lakše = document.getElementById('lako');
+const teže = document.getElementById('teže');
+const kutije = document.querySelectorAll('.kutija');
+const traka = document.querySelector('.osnovnaBoja');
+let kliknuoLakše = false;
+let kliknuoTeže = false;
+let samoTriKutije = [];
+
+lakše.addEventListener('click', function() {
+  kliknuoLakše = true;
+  kliknuoTeže = false;
+  kutije.forEach(kutija => kutija.style.display = 'none');
+  kreirajTriKutije();
+  // console.log(samoTriKutije);
+});
+
+teže.addEventListener('click', function() {
+  samoTriKutije = [];
+  kliknuoTeže = true;
+  kliknuoLakše = false;
+  urediSveKutije();
+  // console.log(kutije);
+});
+
+function findRandomElement (arr) {
+  if (arr && arr.length) {
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+}
+
+function brojZaBoju() {
+  const boja = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+  return boja;
+}
+
+function pogodiBoju(traženiNiz, traženaBoja) {
+  traženiNiz.forEach(novaKutija => {
+    novaKutija.addEventListener('click', function(e) {
+      if(e.target.style.backgroundColor !== traženaBoja) {
+        novaKutija.style.display = 'none';
+      } else {
+        console.log('pogodak');
+        traka.style.backgroundColor = traženaBoja
+        traženiNiz.forEach(novaKutija => {
+          novaKutija.style.backgroundColor = traženaBoja;
+          novaKutija.style.display = '';
+        });    
+      }
+    });
+  });
+}
+
+function kreirajTriKutije() {
+  kutije.forEach(kutija => {
+    if(samoTriKutije.length < 3) samoTriKutije.push(kutija)
+  });
+  samoTriKutije.forEach(novaKutija => {
+    novaKutija.style.backgroundColor = brojZaBoju();
+    novaKutija.style.display = '';
+  });
+  let rand = findRandomElement(samoTriKutije);
+  traka.textContent = rand.style.backgroundColor;
+  pogodiBoju(samoTriKutije, traka.textContent)
+}
+
+
+function urediSveKutije() {
+  kutije.forEach(kutija => {
+    kutija.style.backgroundColor = brojZaBoju();
+    kutija.style.display = '';
+  });
+  let randomElement = findRandomElement(kutije);
+  traka.textContent = randomElement.style.backgroundColor;
+  pogodiBoju(kutije, traka.textContent);
+}
+
+reset.addEventListener('click', function() {
+  (kliknuoLakše) ? kreirajTriKutije() : urediSveKutije();
+});
